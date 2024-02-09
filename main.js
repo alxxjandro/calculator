4be527bb3c6058616e6b5ce28d983a492700948b
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-02-09 17:04:45 -0600

    Added basic functionality to make the numbers print to screen on-click

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 
 const display = document.querySelector('.display')
+const numberBtns = document.querySelectorAll('.number')
 const displayContent = document.createElement('p')
 display.append(displayContent);
 
-function renderToDisplay(render){
-    displayContent.innerText = '';
-    displayContent.innerText = render;
+
+function renderToScreen(number) {
+    if (displayContent.innerText === '0') {
+        displayContent.innerText = number;
+    } else {
+        displayContent.innerText += number;
+    }
 }
 
-renderToDisplay(123132131)
+
+numberBtns.forEach(button =>{
+    button.addEventListener('click', () => {
+        renderToScreen(button.textContent);
+    })
+})
+
+//funciones logicas
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+//operate the calculator
+function operate(numberOne,numberTwo){
+
+}
+
+
+//default
+renderToScreen(0);
+

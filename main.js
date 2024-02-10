26bf9ae9aba801f7ad13271e11523c4ef91729a9
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-02-09 21:12:13 -0600

    The calculator seems to work, but there a couple of bugs, mainly when doing multiple operations

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
-const display = document.querySelector('.display')
-const numberBtns = document.querySelectorAll('.number')
-const displayContent = document.createElement('p')
-display.append(displayContent);
+const display = document.querySelector('.display');
+const numberBtns = document.querySelectorAll('.number');
+const displayContent = document.createElement('p');
+    display.append(displayContent);
+
+
+const operatorBtns = document.querySelectorAll('.operator')
+const equalBtn = document.querySelector('#equal');
+let firstNumber = null,
+    operator = null;
 
 
 function renderToScreen(number) {
@@ -13,12 +19,53 @@ function renderToScreen(number) {
     }
 }
 
+function getNumber(){
+    return parseInt(displayContent.innerText);
+}
+
+operatorBtns.forEach(button =>{
+    button.addEventListener('click', () => {
+        firstNumber = getNumber(); 
+        displayContent.innerText = '';
+        console.log(firstNumber)
+        renderToScreen('');
+        operator = button.textContent
+        return firstNumber,operator;
+    })
+})
 
 numberBtns.forEach(button =>{
     button.addEventListener('click', () => {
         renderToScreen(button.textContent);
     })
 })
+
+equalBtn.addEventListener('click', () => {
+    const secondNumber = getNumber();
+    console.log(firstNumber,secondNumber)
+    let result;
+    switch(operator){
+        case '+':
+            result = firstNumber + secondNumber;
+            break
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case '/':
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            result = 'ERROR'
+            break
+    }
+    displayContent.innerText = result;
+    firstNumber = result;
+});
+
+
 
 //funciones logicas
 function add(a,b){
